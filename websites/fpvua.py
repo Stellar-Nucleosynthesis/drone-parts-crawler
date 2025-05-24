841c6a381f536f6a561f16094e394cdc2e2da28a
Stellar-Nucleosynthesis <stepanenkonazar356@gmail.com>
2025-05-24 14:04:11 +0300

    Increased scanning precision (multiple names of a characteristic allowed)

--- websites/fpvua.py
+++ websites/fpvua.py
@@ -13,17 +13,20 @@ def detail_url_finder(sell_page):
     soup = BeautifulSoup(sell_page.text, "html.parser")
     return [product.find('a')['href'] for product in soup.select('div.single-product[id^="product-card-"]')]
 
-def find_attr(page, name):
+def find_attr(page, values):
     soup = BeautifulSoup(page.text, "html.parser")
     for li in soup.find_all("li"):
         span = li.find("span")
-        if span and span.text.strip() == name:
+        if span and span.text.strip() in values:
             p = li.find("p")
             if p:
                 return p.text.strip()
     return None
 
 def find_detail_model(page):
+    res = find_attr(page, ["Модель"])
+    if res:
+        return res
     try:
         soup = BeautifulSoup(page.text, "html.parser")
         container = soup.find("div", class_="product-container")
@@ -33,6 +36,9 @@ def find_detail_model(page):
         return None
 
 def find_detail_manufacturer(page):
+    res = find_attr(page, ["Виробник"])
+    if res:
+        return res
     soup = BeautifulSoup(page.text, "html.parser")
     for li in soup.find_all("li"):
         span = li.find("span")
@@ -46,55 +52,53 @@ def find_detail_manufacturer(page):
 
 attr_parsers = {}
 
-frame_parsers = {}
-frame_parsers["model"] = find_detail_model
-frame_parsers["manufacturer"] = find_detail_manufacturer
-frame_parsers["propellers_inches"] = lambda text: find_attr(text, "Розмір пропелерів")
-frame_parsers["mass"] = lambda text: find_attr(text, "Вага")
-frame_parsers["material"] = lambda text: find_attr(text, "Матеріал")
-frame_parsers["size_mm"] = lambda text: find_attr(text, "Розмір рами")
-frame_parsers["cam_mount_size"] = lambda text: find_attr(text, "Розмір кріплення камери")
-frame_parsers["motor_mount_size"] = lambda text: find_attr(text, "Отвір для кріплення двигуна")
-attr_parsers["Frame"] = frame_parsers
+frame = {}
+frame["model"] = find_detail_model
+frame["manufacturer"] = find_detail_manufacturer
+frame["propellers_inches"] = lambda text: find_attr(text, ["Розмір пропелерів"])
+frame["mass"] = lambda text: find_attr(text, ["Вага"])
+frame["material"] = lambda text: find_attr(text, ["Матеріал"])
+frame["size_mm"] = lambda text: find_attr(text, ["Розмір рами", "Розмір (діагональ між центрами моторів)"])
+frame["cam_mount_size"] = lambda text: find_attr(text, ["Розмір кріплення камери"])
+frame["motor_mount_size"] = lambda text: find_attr(text, ["Отвір для кріплення двигуна"])
+attr_parsers["Frame"] = frame
 
-propeller_parsers = {}
-propeller_parsers["model"] = find_detail_model
-propeller_parsers["manufacturer"] = find_detail_manufacturer
-propeller_parsers["material"] = lambda text: find_attr(text, "Матеріал")
-propeller_parsers["propeller_size"] = lambda text: find_attr(text, "Розмір пропелера")
-attr_parsers["Propeller"] = propeller_parsers
+prop = {}
+prop["model"] = find_detail_model
+prop["manufacturer"] = find_detail_manufacturer
+prop["material"] = lambda text: find_attr(text, ["Матеріал"])
+prop["propeller_size"] = lambda text: find_attr(text, ["Розмір пропелера", "Розмір"])
+attr_parsers["Propeller"] = prop
 
-camera_parsers = {}
-camera_parsers["model"] = find_detail_model
-camera_parsers["manufacturer"] = find_detail_manufacturer
-camera_parsers["mass"] = lambda text : find_attr(text, "Вага")
-camera_parsers["size_mm"] = lambda text : find_attr(text, "Розмір")
-camera_parsers["tvl"] = lambda text : find_attr(text, "Горизонтальна роздільна здатність")
-camera_parsers["aspect_ratio"] = lambda text : find_attr(text, "Співвідношення сторін")
-camera_parsers["video_format"] = lambda text : find_attr(text, "Система сигналу")
-attr_parsers["Camera"] = camera_parsers
+camera = {}
+camera["model"] = find_detail_model
+camera["manufacturer"] = find_detail_manufacturer
+camera["mass"] = lambda text : find_attr(text, ["Вага"])
+camera["size_mm"] = lambda text : find_attr(text, ["Розмір"])
+camera["tvl"] = lambda text : find_attr(text, ["Горизонтальна роздільна здатність"])
+camera["aspect_ratio"] = lambda text : find_attr(text, ["Співвідношення сторін"])
+camera["video_format"] = lambda text : find_attr(text, ["Система сигналу"])
+attr_parsers["Camera"] = camera
 
-vtx_parsers = {}
+vtx = {}
 def find_vtx_power(text):
-    res = find_attr(text, "Вихідна потужність")
+    res = find_attr(text, ["Вихідна потужність"])
     if res:
         return res
-    else:
-        return (m.group() if (m := re.search(r'\b[\d.]+m?W\b', find_detail_model(text))) else None)
+    return (m.group() if (m := re.search(r'\b[\d.]+m?W\b', find_detail_model(text))) else None)
 
 def find_vtx_frequency(text):
-    res = find_attr(text, "Частота відеопередавача")
+    res = find_attr(text, ["Частота відеопередавача"])
     if res:
         return res
-    else:
-        return (m.group() if (m := re.search(r'\b[\d.]+G\b', find_detail_model(text))) else None)
+    return (m.group() if (m := re.search(r'\b[\d.]+G\b', find_detail_model(text))) else None)
 
-vtx_parsers["model"] = find_detail_model
-vtx_parsers["manufacturer"] = find_detail_manufacturer
-vtx_parsers["connector"] = lambda text : find_attr(text, "Роз'єм антенни")
-vtx_parsers["max_power"] = find_vtx_power
-vtx_parsers["video_format"] = lambda text : find_attr(text, "Відеоформат")
-vtx_parsers["mass"] = lambda text : find_attr(text, "Вага")
-vtx_parsers["size_mm"] = lambda text : find_attr(text, "Розмір")
-vtx_parsers["frequency"] = find_vtx_frequency
-attr_parsers["VTX"] = vtx_parsers
+vtx["model"] = find_detail_model
+vtx["manufacturer"] = find_detail_manufacturer
+vtx["connector"] = lambda text : find_attr(text, ["Роз'єм антенни", "Інтерфейс антени"])
+vtx["max_power"] = find_vtx_power
+vtx["video_format"] = lambda text : find_attr(text, ["Відеоформат"])
+vtx["mass"] = lambda text : find_attr(text, ["Вага"])
+vtx["size_mm"] = lambda text : find_attr(text, ["Розмір"])
+vtx["frequency"] = find_vtx_frequency
+attr_parsers["VTX"] = vtx
